f0002e6371dd181f373d90b3c5357a9ef6f77170
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-09-08 18:25:15 +0200

    fixed directory bug

--- app/create_file.py
+++ app/create_file.py
@@ -17,32 +17,32 @@ def add_content(output_file: TextIO) -> None:
         i += 1
 
 
-def create_file() -> None:
+def create_file(path: str) -> None:
     file_name = sys.argv[sys.argv.index("-f") + 1]
 
     if os.path.exists(file_name):
-        with open(file_name, "a") as output_file:
+        with open(os.path.join(path, file_name), "a") as output_file:
             output_file.write(
                 "\n" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
             add_content(output_file)
     else:
-        with open(file_name, "w") as output_file:
+        with open(os.path.join(path, file_name), "w") as output_file:
             output_file.write(
                 datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
             add_content(output_file)
 
 
-def create_directory(border: int) -> None:
+def create_directory(border: int) -> str:
     path = os.path.join(
         os.getcwd(), *sys.argv[sys.argv.index("-d") + 1: border])
     os.makedirs(path)
-    os.chdir(path)
+    return path
 
 
 if "-f" in sys.argv and "-d" in sys.argv:
-    create_directory(sys.argv.index("-f"))
-    create_file()
+    path = create_directory(sys.argv.index("-f"))
+    create_file(path)
 elif "-f" in sys.argv:
-    create_file()
+    create_file(os.getcwd())
 elif "-d" in sys.argv:
     create_directory(len(sys.argv))
